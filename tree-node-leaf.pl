39b9d36175bee527b32ea3ef3ca477dc08792ae7
gillchristian <gillchristiang@gmail.com>
2016-11-06 11:57:45 -0300

    Update tree basic case.

--- tree-node-leaf.pl
+++ tree-node-leaf.pl
@@ -1,24 +1,30 @@
 % leaf
-%  outher most nodes of the tree
-%  functionality: accumulate chips.
+%   outher most nodes of the tree
+%   functionality: accumulate chips.
 leaf(ID, N) :-
   integer(ID), integer(N), % both numbers must be integers
   ID > -1, % id: greatter than 1
   N > -1. % amount of chips: greatter than 1
 
 % node:
-%  inner nodes of the tree
-%  state: 1 chip follows to the left, 0 chip follows to the right
+%   inner nodes of the tree
+%   state: 1 chip follows to the left, 0 chip follows to the right
 node(ID, S) :-
   integer(ID), integer(S), % both numbers must be integers
   ID > -1, % id: greatter than 1
   S = 1 ; S = 0. % state of the node
 
-% tree: a complete binary tree
-tree(leaf(ID, N), nil, nil) :-
-  leaf(ID, N). % tree with a leaf
+% tree
+%   a complete binary tree
 
-tree(node(ID, N), tree(A, B, C), tree(D, E, F)) :-
+% basic tree: an inner node and two leaves.
+tree(node(ID, N), leaf(IDl, Nl), leaf(IDr, Nr)) :-
+  node(ID, N),
+  leaf(IDl, Nl),
+  leaf(IDr, Nr).
+
+% inner tree: an inner node and two trees.
+tree(node(ID, N), tree(NodeL, LTl, LTr), tree(NodeR, RTl, RTr)) :-
   node(ID, N), % node
-  tree(A, B, C), % left tree
-  tree(D, E, F). % right tree
+  tree(NodeL, LTl, LTr), % left tree
+  tree(NodeR, RTl, RTr). % right tree
