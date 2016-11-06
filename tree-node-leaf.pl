1fb1de9f95b3defda22a5cb25953e95e1b05c9b9
gillchristian <gillchristiang@gmail.com>
2016-11-06 11:27:46 -0300

    Add comments.

--- tree-node-leaf.pl
+++ tree-node-leaf.pl
@@ -1,12 +1,24 @@
 % leaf
-leaf(ID, N) :- integer(ID), integer(N), ID > -1, N > -1.
+%  outher most nodes of the tree
+%  functionality: accumulate chips.
+leaf(ID, N) :-
+  integer(ID), integer(N), % both numbers must be integers
+  ID > -1, % id: greatter than 1
+  N > -1. % amount of chips: greatter than 1
 
-% node
-node(ID, S) :- integer(ID), integer(S), ID > -1, S = 1 ; S = 0.
+% node:
+%  inner nodes of the tree
+%  state: 1 chip follows to the left, 0 chip follows to the right
+node(ID, S) :-
+  integer(ID), integer(S), % both numbers must be integers
+  ID > -1, % id: greatter than 1
+  S = 1 ; S = 0. % state of the node
 
-% tree
-tree(leaf(ID, N), nil, nil) :- leaf(ID, N).
+% tree: a complete binary tree
+tree(leaf(ID, N), nil, nil) :-
+  leaf(ID, N). % tree with a leaf
+
 tree(node(ID, N), tree(A, B, C), tree(D, E, F)) :-
-  node(ID, N),
-  tree(A, B, C),
-  tree(D, E, F).
+  node(ID, N), % node
+  tree(A, B, C), % left tree
+  tree(D, E, F). % right tree
--- tree-node-leaf.test.pl
+++ tree-node-leaf.test.pl
@@ -1,13 +1,17 @@
+% a basic tree with just a leaf.
 tree(leaf(1,2), nil, nil).
 
+% A: a tree with a node and two basic trees.
 tree(node(1, 1), tree(leaf(1,2), nil, nil), tree(leaf(1,2), nil, nil)).
 
+% B: a tree with a node and two A trees on each side.
 tree(
   node(1, 1),
   tree(node(1, 1), tree(leaf(1,2), nil, nil), tree(leaf(1,2), nil, nil)),
   tree(node(1, 1), tree(leaf(1,2), nil, nil), tree(leaf(1,2), nil, nil))
 ).
 
+% a tree with a node and two B trees on each side.
 tree(
   node(1, 1),
   tree(
