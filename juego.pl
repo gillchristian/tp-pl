a310d41b0d19640a1e253572a8ec2bebbada49bb
gillchristian <gillchristiang@gmail.com>
2016-11-11 09:57:26 -0300

    Update files reference on docs.

--- juego.pl
+++ juego.pl
@@ -10,31 +10,31 @@
 % de esta forma se puede analizar cada módulo independientemente
 %
 % Modulo Máquina
-%   Archivo: tree-node-leaf.pl
+%   Archivo: src/tree-node-leaf.pl
 %   Contiene las definiciones de la maquina, los nodos y las troneras.
 
 :- use_module('src/tree-node-leaf').
 
 % Modulo Generador
-%   Archivo: generate-tree.pl
+%   Archivo: src/generate-tree.pl
 %   Expone la funcion MaquinaEstadoInicial, encargada de generar una máquina.
 
 :- use_module('src/generate-tree').
 
 % Modulo Agregar
-%   Archivo: add-chip.pl
+%   Archivo: src/add-chip.pl
 %   Expone la funcion que permite agregar una ficha en la máquina.
 
 :- use_module('src/add-chip').
 
 % Modulo AgregarMultiple
-%   Archivo: add-n-chips.pl
+%   Archivo: src/add-n-chips.pl
 %   Expone la funcion que permite agregar N fichas en la máquina.
 
 :- use_module('src/add-n-chips').
 
 % Modulo Resultado
-%   Archivo: result.pl
+%   Archivo: src/result.pl
 %   Expone la funcion que permite agregar N fichas en la máquina y
 %   obtener los resultados.
 
